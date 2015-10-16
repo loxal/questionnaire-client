e886d412551e347847acb02882b7c689252206ab
Alex <alexander.orlov@loxal.net>
2015-10-16 14:17:40 +0200

    cleanup code

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,40 +3,8 @@ import {Component, bootstrap, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/a
 @Component({
     directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
     selector: 'app',
-    styles: [`
-      .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-  .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-  .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-  .heroes .badge {
-    font-size: small;
-    color: white;
-    padding: 0.1em 0.7em;
-    background-color: #369;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -1px;
-   }
-   .selected { background-color: #EEE; color: #369; }
-    `],
-    template: `
-       <h1>{{title}}</h1>
- <div *ng-if="selectedHero">
-   <h2>{{selectedHero.name}} details!</h2>
-   <div><label>id: </label>{{selectedHero.id}}</div>
-   <div>
-     <label>name: </label>
-     <input [(ng-model)]="selectedHero.name" placeholder="name"/>
-   </div>
- </div>
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-<li *ng-for="#hero of heroes"
-  [ng-class]="getSelectedClass(hero)"
-  (click)="onSelect(hero)">
-  <span class="badge">{{hero.id}}</span> {{hero.name}}</a>
-</li>
-  </ul>`
+    styleUrls: ["style.css"],
+    templateUrl: "template.html"
 })
 
 class AppComponent {
