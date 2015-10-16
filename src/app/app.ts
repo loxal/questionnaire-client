4e6329a10f97171f81898260921a9084350dd62a
Alex <alexander.orlov@loxal.net>
2015-10-16 04:33:22 +0200

    implement Hero tutorial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,32 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
+    directives: [FORM_DIRECTIVES],
     selector: 'app',
-    template: '<h1>Questionnaire</h1>'
+    template: `
+       <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <div>
+    <input [(ng-model)]="hero.name" placeholder="name"/>
+    </div>
+  </div>
+      `
 })
 
 class AppComponent {
+    public title = 'Tour of Heroes';
+    public hero:Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+
+class Hero {
+    id:number;
+    name:string;
 }
 
 bootstrap(AppComponent);
