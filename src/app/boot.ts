e89d7d77ccc2c7dbfc4f8615f74e842891aeb937
Alex <alexander.orlov@loxal.net>
2016-02-07 20:24:59 +0100

    cleanup project

--- src/app/boot.ts
+++
@@ -1,6 +0,0 @@
-import {enableProdMode} from "angular2/core";
-import {bootstrap}    from "angular2/platform/browser"
-import {Questionnaire} from "./app.component"
-
-//enableProdMode();
-bootstrap(Questionnaire);
---
+++ src/main/kotlin/net/loxal/client/Main.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2016 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ */
+
+package net.loxal.client
+
+
+fun main(vararg args: String) {
+    console.log("2342!cdssdfdsfsdfdsfol!!!!!!!!!!!!ol $args")
+}
